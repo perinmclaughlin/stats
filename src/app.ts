f715374d3d4ace46598d00f93e1930651127a8c0
ariovistus <ellery-newcomer@utulsa.edu>
2018-01-17 17:51:54 -0800

    set up router

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+import { autoinject } from "aurelia-framework";
+import { Router, RouterConfiguration } from "aurelia-router";
+import { PLATFORM } from "aurelia-pal";
+
+@autoinject
 export class App {
   message = 'Hello World!';
+
+  constructor(public router: Router) {
+  }
+
+
+  public activate() {
+    return this.router.configure(this.configureRoutes);
+  }
+
+  public configureRoutes(config: RouterConfiguration): RouterConfiguration {
+    config.map([
+        {
+            route: [""], name: "mainpage", moduleId: PLATFORM.moduleName("mainpage"), 
+            nav: false, title: "Big honking page", adminRoute: false,
+        },
+        {
+            route: ["page1"], name: "page1", moduleId: PLATFORM.moduleName("page1"), 
+            nav: false, title: "Smaller honking page", adminRoute: false,
+        },
+    ]);
+
+    return config;
+  }
+
 }
