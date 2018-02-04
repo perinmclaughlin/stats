edb9e57f9a5394423d47b5dc50bbf605672d8a85
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2018-02-03 16:34:39 -0800

    touch up code

--- src/event-teams.ts
+++ src/event-teams.ts
@@ -1,57 +1,32 @@
+import {autoinject} from "aurelia-framework"
 import {EventTeamData} from "./model";
 import { EventEntity, FrcStatsContext, EventTeamEntity, TeamEntity, EventMatchEntity } from "./persistence";
-import {autoinject} from "aurelia-framework"
 
 @autoinject
 export class EventTeams {
   public event: EventEntity;
   public eventMatches: EventMatchEntity[];
   public teams: {team: TeamEntity, eventTeam: EventTeamEntity}[];
-  constructor(private dbContext: FrcStatsContext){
-    /* this.event = {
-      code: "wayak",
-      name: "Sundome",
-      districtCode: "PNW",
-      year: "2018",
 
-    }; */
+  constructor(private dbContext: FrcStatsContext){
     this.teams = [];
-    /* var team = new EventTeamData();
-    team.teamNumber = "1234";
-    team.matchCount = 1;
-    team.failureCount = 2;
-    team.scale = true;
-    team.switch_cap = true;
-    team.vault = false;
-    team.foulCount = 3;
-    team.cubeAverage = 4;
-    team.eventCode = "wayak";
-    this.teams.push(team); */
-    this.eventMatches[0].
+    this.eventMatches = [];
   }
    
   activate(params){
-	  var i = 0;
-	  this.dbContext.events.where(["year", "eventCode"]).equals([params.year, params.eventCode]).first().then(event =>{
-		  var linesForDays2 = 0;
+	  this.dbContext.events.where(["year", "eventCode"]).equals([params.year, params.eventCode]).first().then(event => {
 		  this.event = event;
     });
-    this.dbContext.eventMatches.where(["year", "eventCode"]).equals([params.year, params.eventCode]).toArray().then(eventMatches =>{
-		  var linesForDays = 0;
+    this.dbContext.eventMatches.where(["year", "eventCode"]).equals([params.year, params.eventCode]).toArray().then(eventMatches => {
 		  this.eventMatches = eventMatches;
 	  });
-	  return this.dbContext.eventTeams.where(["year", "eventCode"]).equals([params.year, params.eventCode]).toArray().then(eventTeams =>{
-		  //this.team = team;
-		  var thisLine = 0;
+	  return this.dbContext.eventTeams.where(["year", "eventCode"]).equals([params.year, params.eventCode]).toArray().then(eventTeams => {
 		  eventTeams.forEach(eventTeam =>{
 			  var anotherLine = 0;
-			 this.dbContext.teams.where("teamNumber").equals(eventTeam.teamNumber).first().then(team =>{
+			 this.dbContext.teams.where("teamNumber").equals(eventTeam.teamNumber).first().then(team => {
 				this.teams.push({team: team, eventTeam: eventTeam});
 			 });
-			  
 		  });
-		  
-		  var lllll = 0;
 	  });
   }
 }
