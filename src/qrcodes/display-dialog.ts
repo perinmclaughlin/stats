429d8026427d92ef253031a9ffbdbd0a0e517671
Spazdiesfirst360 <30324839+Spazdiesfirst360@users.noreply.github.com>
2018-10-16 16:35:58 -0700

    Update display-dialog.ts
    That's right: I forgot Git hates me.

--- src/qrcodes/display-dialog.ts
+++ src/qrcodes/display-dialog.ts
@@ -17,16 +17,32 @@ export class QrCodeDisplayDialog {
 
     click() {
         // I think qrcode-generator is generating Model 2 qr codes? http://www.qrcode.com/en/codes/model12.html
-        var qrType : TypeNumber = 25; 
+        var passIn = [];
+        for(var i = 0; i < 10; i++){
+            passIn.push(i);
+        }
+        
+        var qrType : TypeNumber = 12; 
         var errorCorrection : ErrorCorrectionLevel = 'M';
         var mode: Mode = "Byte";
         var cellSize = 4; // default is 2
         var margin = 10; // default is cellSize * 4
-        var data = "tacostacos";
+        var data = passIn.join("");
 
         var x = qrcode(qrType, errorCorrection);
         x.addData(data, mode);
         x.make();
         this.qrCodeElement.innerHTML = x.createImgTag(cellSize, margin);
+    }
+
+    obtainData(data: string, chunkSize: number) {
+        // Convert data.
+        var data2 = data;
+        var array = [];
+        for(var i = 0; i < Math.ceil(data2.length / chunkSize); i++){
+            var printThis = data2.substr(chunkSize * (i), chunkSize);
+            array.push(printThis);
+        }
+        return(array);
     }
 }
